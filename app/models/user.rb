de1fa4e6ca337a670a4375d6e2248911f193180e
Low Yong Cheng (YC) <lowyongcheng@hotmail.com>
2021-12-06 23:39:37 +0800

    Add authentication module (#23)
    * User: Add devise templates
    
    * User: Add firstname and lastname to users
    
    * User: Add roles to devise

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum role: %I[traveler host]
+
+  after_initialize(:set_default_role, { if: :new_record? })
+
+  def set_default_role
+    self.role ||= :traveler
+  end
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
