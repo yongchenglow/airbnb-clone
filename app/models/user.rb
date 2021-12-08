70373aff950ef5c5e3367b2ede7670dcc3f1399f
Yong Cheng Low <lowyongcheng@hotmail.com>
2021-12-08 18:43:17 +0800

    User: Adjust user roles to give people more flexibility

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  enum role: %I[traveler host]
+  enum role: %I[member admin]
 
   after_initialize(:set_default_role, { if: :new_record? })
 
   def set_default_role
-    self.role ||= :traveler
+    self.role ||= :member
   end
 
   devise :database_authenticatable, :registerable,
