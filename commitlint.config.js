20afe285ad4178656b446dce0a1cf549038b8bcb
Yong Cheng Low <lowyongcheng@hotmail.com>
2021-12-08 15:54:25 +0800

    Revert "Flat: Add Flat model"
    This reverts commit 485d4981bf96e0d6d9a2eabf6955bca359537bcb.

--- commitlint.config.js
+++ commitlint.config.js
@@ -18,7 +18,6 @@ module.exports = {
         "Housekeeping",
         "Config",
         "User",
-        "Flat",
       ],
     ],
   },
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_154252) do
+ActiveRecord::Schema.define(version: 2021_12_06_140541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "flats", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.string "address"
-    t.integer "bed"
-    t.integer "bath"
-    t.integer "bedroom"
-    t.decimal "price"
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_flats_on_user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -44,5 +30,4 @@ ActiveRecord::Schema.define(version: 2021_12_06_154252) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "flats", "users"
 end
