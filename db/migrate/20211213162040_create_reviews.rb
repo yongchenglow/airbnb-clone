916197cdcdec9fa7bace6b22b88afc4bc019af06
Yong Cheng Low <lowyongcheng@hotmail.com>
2021-12-14 00:44:42 +0800

    Flat: Add unique constraint to reviews

--- db/migrate/20211213162040_create_reviews.rb
+++ db/migrate/20211213162040_create_reviews.rb
@@ -4,9 +4,11 @@ class CreateReviews < ActiveRecord::Migration[6.1]
       t.integer :rating, null: false
       t.text :content, null: true
       t.references :user, null: false, foreign_key: true, index: true
-      t.references :flat, null: false, foreign_key: true, index: true
-
+      t.references :booking, null: false, foreign_key: true, index: true
       t.timestamps
     end
+
+    add_index :user, :booking             unique: true
   end
+
 end
