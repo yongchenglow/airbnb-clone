889c3da3697a27904e74ec46f6d806112436932e
Yong Cheng Low <lowyongcheng@hotmail.com>
2021-12-19 22:24:54 +0800

    Favourite: Add favourite controllers and views

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,4 +1,13 @@
 class Favourite < ApplicationRecord
   belongs_to :user
-  has_one :flat
+  belongs_to :flat
+
+  include PgSearch::Model
+  pg_search_scope :search_flats_by_title_or_description_or_address,
+                  associated_against: {
+                    flat: %I[title description address]
+                  },
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
   resources :flats do
     resources :bookings, only: [ :new, :create ]
   end
+
   resources :bookings, only: [ :new, :create ] do
     resources :reviews, only: [ :new, :create ]
   end
+
+  resources :favourites, only: [ :index, :new, :create, :destroy]
   resources :bookings, only: [ :destroy ]
   resources :reviews, only: [ :destroy ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
