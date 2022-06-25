56e15c7a33d7080391160a5f7663b4a3aabc68c5
Low Yong Cheng (YC) <lowyongcheng@hotmail.com>
2022-06-25 09:47:54 +0800

    Config: Add eslint (#51)

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    semi: 'error',
+  },
+};
