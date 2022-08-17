d315cbe25b5be6216dd63d6ade8c3fa6ccfd2896
Low Yong Cheng (YC) <lowyongcheng@hotmail.com>
2022-08-17 18:09:51 +0800

    Config: Add messages to hooks (#11)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  "*.{js,jsx,ts,tsx,erb,rb,md,mdx,css,yaml,yml}": [
-    "prettier --write",
-    "git add",
-  ],
+  "*.{js,jsx,ts,tsx,md,mdx,css,yaml,yml}": ["prettier --write", "git add"],
+  "*.{erb,rb}": ["prettier --write", "bundle exec rubocop -a", "git add"],
 };
