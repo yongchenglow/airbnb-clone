7ba5282d2f52b8c78e3065bf238f0ba1703e9cb3
yong cheng low <lowyongcheng@hotmail.com>
2022-08-19 20:29:47 +0800

    User: Add basic devise template

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
-Rails.application.routes.draw do
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+Rails
+  .application
+  .routes
+  .draw do
+    devise_for :users
+    root to: 'pages#home'
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
+    # Defines the root path route ("/")
+    # root "articles#index"
+  end
