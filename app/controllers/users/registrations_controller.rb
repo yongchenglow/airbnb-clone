ddb79299f96afd69e74af72ab8502a375409733b
yong cheng low <lowyongcheng@hotmail.com>
2022-08-22 00:00:58 +0800

    User: Add show testing page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,9 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
     layout "centered"
+
+    def show
+    end
     # before_action :configure_sign_up_params, only: [:create]
     # before_action :configure_account_update_params, only: [:update]
     # GET /resource/sign_up
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
                sign_in: "login" # Use user/login instead of sign_in
              }
 
+  devise_scope :user do
+    get "users/:id", to: "users/registrations#show", as: :user
+  end
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
