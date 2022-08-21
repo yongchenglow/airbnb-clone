102f6a5e8e879960012d6c19afe2055ff194e81c
yong cheng low <lowyongcheng@hotmail.com>
2022-08-21 23:24:27 +0800

    User: Moved Avatar to atoms

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,9 @@ Rails.application.routes.draw do
                sessions: "users/sessions",
                unlocks: "users/unlocks"
              },
-             path: "user", # User user instead of users which makes a lot more sense
              path_names: {
                sign_in: "login" # Use user/login instead of sign_in
              }
-
-  # This allows for /profile to work, makes a lot more sense compared to users/edit
-  devise_scope :user do
-    get "profile", to: "users/registrations#edit" # Take note that this is the path to the controller
-  end
 
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
